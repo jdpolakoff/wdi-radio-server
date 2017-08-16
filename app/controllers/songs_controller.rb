d244cd9ea9968ae2c4c39edde5a98020cc97db08
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-16 13:36:25 -0400

    completed first steps

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,12 @@
+class SongsController
+
+  def index
+    @songs = Song.all
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @songs }
+    end
+  end
+
+end
